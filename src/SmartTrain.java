fd12c6a3544fef14f4c5a81f8665843164d7c936
Christopher James Shelton <cjshelton00@cs.unm.edu>
2020-03-24 21:47:13 -0600

    Initial commit

---
+++ src/SmartTrain.java
@@ -0,0 +1,9 @@
+public class SmartTrain {
+
+    public SmartTrain(){
+        System.out.println("Hello Trains");
+    }
+    public static void main(String[] args) {
+        SmartTrain train = new SmartTrain();
+    }
+}
