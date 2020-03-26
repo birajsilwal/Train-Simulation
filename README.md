c4dc558793ff598c4bd82f6208c4e3a48e316189
Christopher James Shelton <cjshelton00@cs.unm.edu>
2020-03-26 11:58:47 -0600

    Added notes

---
+++ README.md
@@ -0,0 +1,16 @@
+Smart Train 
+
+TODO
+- Read in the config file 
+
+- get a list of all of the components
+
+- make the track
+
+- While loop---------------------
+- Make a move
+
+- See if the move makes sense
+
+- move the train
+- End of while loop-------------
--- src/SmartTrain.java
+++ src/SmartTrain.java
@@ -1,7 +1,19 @@
 public class SmartTrain {
 
     public SmartTrain(){
-        System.out.println("Hello Trains");
+        //Read in the config file
+
+        //get a list of all of the components
+
+        //make the track
+
+        //While loop---------------------
+        //Make a move
+
+        //See if the move makes sense
+
+        //move the train
+        //End of while loop-------------
     }
     public static void main(String[] args) {
         SmartTrain train = new SmartTrain();
